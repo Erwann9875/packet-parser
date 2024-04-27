d15e4da0c421c16136e70d25f9fb2366b2609c75
Fizo55 <hugomacees56000@gmail.com>
2024-04-27 16:50:01 +0200

    feat: add fc maps different handling

--- parser/map_parser.py
+++ parser/map_parser.py
@@ -16,11 +16,12 @@ class MapParser:
         generic_parser = GenericParser(folder + self._file_map_id_dat, "DATA 0", 0, [])
         return generic_parser.get_dtos(" ")
 
-    async def insert_or_update_maps_async(self, folder: str, packet_list: List[List[str]]):
+    async def insert_or_update_maps_async(self, fc: bool, folder: str, packet_list: List[List[str]]):
         dictionary_id = self.parse_dat_async(folder)
         folder_map = folder + self._folder_map
         dictionary_music = {x[2]: x[7] for x in packet_list if len(x) > 7 and x[0].strip() == "at"}
         maps = []
+        fc_maps = []
         for file in Path(folder_map).iterdir():
             map_id = int(file.name)
             map_dto = MapDto(
@@ -28,6 +29,14 @@ class MapParser:
                 int(dictionary_music.get(file.name, 0)),
                 map_id == 147
             )
-            maps.append(map_dto)
-        maps_yaml = Converter.dto_to_yaml(maps)
+            # act 4 maps, must be in a separate files
+            if map_id in [130, 131, 132, 133, 134, 151, 152, 153, 154]:
+                fc_maps.append(map_dto)
+            else:
+                maps.append(map_dto)
+        maps_yaml = None
+        if not fc:
+            maps_yaml = Converter.dto_to_yaml(maps)
+        else:
+            maps_yaml = Converter.dto_to_yaml(fc_maps)
         return maps_yaml
--- plugins/map_plugin.py
+++ plugins/map_plugin.py
@@ -2,6 +2,7 @@ from plugin import Plugin
 from parser.generic_parser import GenericParser
 from parser.map_parser import MapParser
 import os
+import shutil
 
 class MapPlugin(Plugin):
     async def process(self, cleaned_lines):
@@ -10,7 +11,8 @@ class MapPlugin(Plugin):
         packet_list = [line for line in cleaned_lines_filtered if line[0] == "at"]
 
         map_parser = MapParser()
-        maps_yaml = await map_parser.insert_or_update_maps_async(folder, packet_list)
+        maps_yaml = await map_parser.insert_or_update_maps_async(False, folder, packet_list)
+        fc_maps_yaml = await map_parser.insert_or_update_maps_async(True, folder, packet_list)
 
         output_maps_dir = "./output/maps"
         if os.path.exists(output_maps_dir):
@@ -20,5 +22,8 @@ class MapPlugin(Plugin):
 
         with open(os.path.join(output_maps_dir, "official_maps.yaml"), "w") as yaml_file:
             yaml_file.write(maps_yaml)
+
+        with open(os.path.join(output_maps_dir, "act4_maps.yaml"), "w") as yaml_file:
+            yaml_file.write(fc_maps_yaml)
 
         print("Map parsed successfully !")
