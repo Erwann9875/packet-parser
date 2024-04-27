79ecb3f7c515854bfd8315f91a1e482d55bc634a
Fizo55 <hugomacees56000@gmail.com>
2024-04-27 17:30:59 +0200

    feat: add flag support on maps

--- dto/map_dto.py
+++ dto/map_dto.py
@@ -1,11 +1,12 @@
 class MapDto:
-    def __init__(self, map_id: int, music: int, shop_allowed: bool):
+    def __init__(self, map_id: int, music: int, shop_allowed: bool, flags: list):
         self.map_id = map_id
         self.map_vnum = map_id
         self.map_name_id = map_id
         self.music = music
         self.shop_allowed = shop_allowed
+        self.flags = flags
 
     def __repr__(self):
         return f"MapDto(map_id={self.map_id}, map_vnum={self.map_vnum}, music={self.music}, " \
-               f"shop_allowed={self.shop_allowed})"
+               f"shop_allowed={self.shop_allowed}, flags={self.flags})"
--- parser/map_parser.py
+++ parser/map_parser.py
@@ -8,6 +8,18 @@ from dto.map_dto import MapDto
 from converter import Converter
 
 class MapParser:
+    FLAG_DEFINITIONS = {
+        130: ["ACT_4", "ANGEL_SIDE", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED", "HAS_SEALED_VESSELS_DISABLED"],
+        131: ["ACT_4", "DEMON_SIDE", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED", "HAS_SEALED_VESSELS_DISABLED"],
+        132: ["ACT_4", "ANGEL_SIDE", "HAS_PVP_FACTION_ENABLED", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED", "HAS_IMMUNITY_ON_MAP_CHANGE_ENABLED", "HAS_SEALED_VESSELS_DISABLED"],
+        133: ["ACT_4", "DEMON_SIDE", "HAS_PVP_FACTION_ENABLED", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED", "HAS_IMMUNITY_ON_MAP_CHANGE_ENABLED", "HAS_SEALED_VESSELS_DISABLED"],
+        134: ["ACT_4", "HAS_PVP_FACTION_ENABLED", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED", "HAS_IMMUNITY_ON_MAP_CHANGE_ENABLED", "HAS_SEALED_VESSELS_DISABLED", "HAS_PVE_REPUTATION_ENABLED"],
+        151: ["ACT_4", "ANGEL_SIDE", "HAS_PVP_FACTION_ENABLED", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED", "HAS_IMMUNITY_ON_MAP_CHANGE_ENABLED", "HAS_SEALED_VESSELS_DISABLED"],
+        152: ["ACT_4", "DEMON_SIDE", "HAS_PVP_FACTION_ENABLED", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED", "HAS_IMMUNITY_ON_MAP_CHANGE_ENABLED", "HAS_SEALED_VESSELS_DISABLED"],
+        153: ["ACT_4", "HAS_PVP_FACTION_ENABLED", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED", "HAS_IMMUNITY_ON_MAP_CHANGE_ENABLED", "HAS_SEALED_VESSELS_DISABLED", "HAS_PVE_REPUTATION_ENABLED"],
+        154: ["ACT_4", "HAS_PVP_FACTION_ENABLED", "HAS_DROP_DIRECTLY_IN_INVENTORY_ENABLED"]
+    }
+
     def __init__(self):
         self._file_map_id_dat = os.path.join(os.path.sep, "MapIDData.dat")
         self._folder_map = os.path.join(os.path.sep, "map")
@@ -24,10 +36,12 @@ class MapParser:
         fc_maps = []
         for file in Path(folder_map).iterdir():
             map_id = int(file.name)
+            flags = self.get_flags(map_id)
             map_dto = MapDto(
                 map_id,
                 int(dictionary_music.get(file.name, 0)),
-                map_id == 147
+                map_id == 147,
+                flags
             )
             # act 4 maps, must be in a separate files
             if map_id in [130, 131, 132, 133, 134, 151, 152, 153, 154]:
@@ -40,3 +54,12 @@ class MapParser:
         else:
             maps_yaml = Converter.dto_to_yaml(fc_maps)
         return maps_yaml
+    
+    def add_map_flag(map_id: int, flag: str):
+        if map_id in FLAG_DEFINITIONS:
+            FLAG_DEFINITIONS[map_id].append(flag)
+        else:
+            FLAG_DEFINITIONS[map_id] = [flag]
+
+    def get_flags(self, map_id):
+        return self.FLAG_DEFINITIONS.get(map_id, [])
