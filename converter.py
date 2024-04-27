8fae0221c65716c7d4c599a2c742bb957af9fc08
Fizo55 <hugomacees56000@gmail.com>
2024-04-27 23:08:08 +0200

    chore: fix yaml serializer and add map flags

--- converter.py
+++ converter.py
@@ -7,4 +7,4 @@ class Converter:
         for dto in dtos:
             dto_dict = dto.__dict__
             yaml_data.append(dto_dict)
-        return yaml.dump(yaml_data, default_flow_style=False)
+        return yaml.dump(yaml_data, default_flow_style=False, sort_keys=False)
--- dto/map_dto.py
+++ dto/map_dto.py
@@ -1,12 +1,11 @@
 class MapDto:
-    def __init__(self, map_id: int, music: int, shop_allowed: bool, flags: list):
+    def __init__(self, map_id: int, music: int, flags: list):
         self.map_id = map_id
         self.map_vnum = map_id
         self.map_name_id = map_id
         self.music = music
-        self.shop_allowed = shop_allowed
         self.flags = flags
 
     def __repr__(self):
         return f"MapDto(map_id={self.map_id}, map_vnum={self.map_vnum}, music={self.music}, " \
-               f"shop_allowed={self.shop_allowed}, flags={self.flags})"
+               f"flags={self.flags})"
