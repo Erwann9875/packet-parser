3629e800de44c8d3199e2cbf529991213cd3e290
Fizo55 <hugomacees56000@gmail.com>
2024-04-28 16:55:00 +0200

    feat: add monster parser #3

---
+++ parser/monster_parser.py
@@ -0,0 +1,41 @@
+from typing import List
+from dto.monster_dto import MonsterDto
+
+class MonsterParser:
+    def insert_monsters(self, packet_list: List[List[str]]):
+        map_id = 0
+        monsters = []
+        mob_mv_packets_list = {int(packet[2]) for packet in packet_list if packet[0] == "mv" and packet[1] == "3"}
+
+        for current_packet in packet_list:
+            if current_packet[0] == "at" and len(current_packet) > 5:
+                map_id = int(current_packet[2])
+            
+            if current_packet[0] == "in" and current_packet[1] == "3" and int(current_packet[3]) <= 20000:
+                monster_id = int(current_packet[2])
+                map_monster_id = int(current_packet[3])
+                map_x = int(current_packet[4])
+                map_y = int(current_packet[5])
+                direction = int(current_packet[6]) if current_packet[6] else 0
+                is_disabled = False
+                is_moving = map_monster_id in mob_mv_packets_list
+
+                monster = MonsterDto(map_id, map_monster_id, monster_id, map_x, map_y, is_moving)
+                monsters.append(monster)
+            
+            monsters.append(monster)
+        return self.group_monsters_by_map_id(monsters)
+        
+    def group_monsters_by_map_id(self, monsters):
+        monsters_map = {}
+        for monster in monsters:
+            if monster.map_id not in monsters_map:
+                monsters_map[monster.map_id] = []
+            monsters_map[monster.map_id].append({
+                "map_monster_id": monster.map_monster_id,
+                "vnum": monster.vnum,
+                "map_x": monster.map_x,
+                "map_y": monster.map_y,
+                "can_move": monster.can_move
+            })
+        return monsters_map
---
+++ plugins/monster_plugin.py
@@ -0,0 +1,54 @@
+from plugin import Plugin
+from parser.monster_parser import MonsterParser
+from dto.monster_dto import MonsterDto
+import os
+import shutil
+import yaml
+
+class MonsterPlugin(Plugin):
+    def __init__(self):
+        super().__init__()
+        self.monsters = []
+
+    async def process(self, cleaned_lines):
+        cleaned_lines_filtered = [line for line in cleaned_lines if line]
+        packet_list = [line for line in cleaned_lines_filtered if line[0] in ("in", "mv", "at")]
+
+        monster_parser = MonsterParser()
+        monster_groups = monster_parser.insert_monsters(packet_list)
+
+        output_monsters_dir = "./output/monster_data"
+        if os.path.exists(output_monsters_dir):
+            shutil.rmtree(output_monsters_dir)
+        os.makedirs(output_monsters_dir)
+
+        for map_id, monsters_info in monster_groups.items():
+            for monster_info in monsters_info:
+                monster_dto = MonsterDto(map_id=map_id, **monster_info)
+                self.monsters.append(monster_dto)
+
+        self.save_monsters_to_yaml(output_monsters_dir)
+        print("Monster parsed successfully !")
+
+    def save_monsters_to_yaml(self, output_dir: str):
+        for map_id, monsters in self.monsters_by_map_id().items():
+            monsters_info = []
+            for monster in monsters:
+                monsters_info.append({
+                    "map_monster_id": monster.map_monster_id,
+                    "vnum": monster.vnum,
+                    "map_x": monster.map_x,
+                    "map_y": monster.map_y,
+                    "can_move": monster.can_move
+                })
+            yaml_data = yaml.dump({f"map_id": map_id, "monsters": monsters_info}, default_flow_style=False, sort_keys=False)
+            with open(os.path.join(output_dir, f"monsters_{map_id}.yaml"), "w") as yaml_file:
+                yaml_file.write(yaml_data)
+
+    def monsters_by_map_id(self):
+        monsters_map = {}
+        for monster in self.monsters:
+            if monster.map_id not in monsters_map:
+                monsters_map[monster.map_id] = []
+            monsters_map[monster.map_id].append(monster)
+        return monsters_map
