f36412fbf121b0155dfeebf0e73cad3cef0a05f9
Fizo55 <hugomacees56000@gmail.com>
2024-05-02 16:12:48 +0200

    feat: add npc recipe scroll

--- parser/recipe_parser.py
+++ parser/recipe_parser.py
@@ -6,6 +6,7 @@ class RecipeParser:
         map_npc_id = 0
         item_vnum = 0
         recipes = []
+        producer_item_vnum = 0
 
         for current_packet in packet_list:
             if len(current_packet) > 2 and current_packet[0] == "pdtse":
@@ -16,12 +17,20 @@ class RecipeParser:
                 map_npc_id = int(current_packet[4])
                 continue
 
-            if current_packet[0] == "m_list" and (current_packet[1] == "2" or current_packet[1] == "4"):
+            if current_packet[0] == "m_list" and current_packet[1] == "3":
                 items = []
+                for i in range(2, len(current_packet), 2):
+                    if int(current_packet[i + 1]) != -1:
+                        vnum = int(current_packet[i + 1])
+                        quantity = int(current_packet[i])
+                        items.append({"item_vnum": vnum, "quantity": quantity})
 
-                for i in range(2, len(current_packet) - 1):
-                    vnum = int(current_packet[i])
-                    items.append(vnum)
-                
-                recipe = RecipeDto(item_vnum)
-                
+                recipe = next((r for r in recipes if r.item_vnum == item_vnum), None)
+                if recipe is None:
+                    recipe = RecipeDto(item_vnum=item_vnum, quantity=1, items=items,
+                                       producer_map_npc_id=map_npc_id, producer_item_vnum=producer_item_vnum)
+                    recipes.append(recipe)
+                else:
+                    recipe.items.extend(items)
+
+        return recipes
--- plugins/recipe_plugin.py
+++ plugins/recipe_plugin.py
@@ -1,4 +1,5 @@
 from plugin import Plugin
+from collections import defaultdict
 from parser.recipe_parser import RecipeParser
 import os
 import shutil
@@ -10,24 +11,35 @@ class RecipePlugin(Plugin):
         packet_list = [line for line in cleaned_lines_filtered if line[0] in ("pdtse", "m_list", "pdtclose", "n_run")]
 
         recipe_parser = RecipeParser()
-        recipe_groups = recipe_parser.insert_recipes(packet_list)
+        recipes = recipe_parser.insert_recipes(packet_list)
+
+        recipe_groups = defaultdict(list)
+        for recipe in recipes:
+            recipe_groups[recipe.producer_map_npc_id].append(recipe)
 
         output_recipes_dir = "./output/recipes"
         if os.path.exists(output_recipes_dir):
             shutil.rmtree(output_recipes_dir)
         os.makedirs(output_recipes_dir)
 
-        # for producer_item_vnum, recipes in recipe_groups.items():
-        #     yaml_data = self.custom_dto_to_yaml(recipes)
-        #     with open(os.path.join(output_recipes_dir, f"recipes_{producer_item_vnum}.yaml"), "w") as yaml_file:
-        #         yaml_file.write(yaml_data)
+        for npc_id, recipes in recipe_groups.items():
+            if npc_id is not None:
+                yaml_data = self.recipes_to_yaml(recipes)
+                file_path = os.path.join(output_recipes_dir, f"npc_{npc_id}_recipes.yaml")
+                with open(file_path, "w") as yaml_file:
+                    yaml_file.write(yaml_data)
 
         print("Recipes parsed successfully !")
 
-    # def custom_dto_to_yaml(self, dtos):
-    #     yaml_data = {'recipes': []}
-    #     for dto in dtos:
-    #         for recipe in dto['recipes']:
-    #             cleaned_recipe = {key: value for key, value in recipe.items() if key in ['item_vnum', 'quantity', 'producer_map_npc_id', 'items']}
-    #             yaml_data['recipes'].append(cleaned_recipe)
-    #     return yaml.dump(yaml_data, default_flow_style=False, sort_keys=False)
+    def recipes_to_yaml(self, recipes):
+        yaml_list = []
+        for dto in recipes:
+            recipe_dict = {
+                "item_vnum": dto.item_vnum,
+                "quantity": dto.quantity,
+                "producer_map_npc_id": dto.producer_map_npc_id,
+                #"producer_item_vnum": dto.producer_item_vnum,
+                "items": dto.items
+            }
+            yaml_list.append(recipe_dict)
+        return yaml.dump({"recipes": yaml_list}, default_flow_style=False, sort_keys=False)
