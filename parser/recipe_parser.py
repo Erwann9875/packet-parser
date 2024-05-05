c411710e76f5181d01e39f7fa5798b0132a93620
Fizo55 <hugomacees56000@gmail.com>
2024-05-05 16:19:53 +0200

    feat: add all kind of missed recipes

--- parser/recipe_parser.py
+++ parser/recipe_parser.py
@@ -7,8 +7,12 @@ class RecipeParser:
         item_vnum = 0
         recipes = []
         producer_item_vnum = 0
+        is_item = False
 
         for current_packet in packet_list:
+            if len(current_packet) > 1 and current_packet[0] == "##" and current_packet[1] == "scroll":
+                is_item = True
+
             if len(current_packet) > 2 and current_packet[0] == "pdtse":
                 item_vnum = int(current_packet[2])
                 continue
@@ -17,7 +21,11 @@ class RecipeParser:
                 map_npc_id = int(current_packet[4])
                 continue
 
-            if current_packet[0] == "m_list" and current_packet[1] == "3":
+            if current_packet[0] == "m_list" and current_packet[1] == "2" and is_item:
+                producer_item_vnum = int(current_packet[2])
+                map_npc_id = None
+
+            if current_packet[0] == "m_list" and (current_packet[1] == "3" or current_packet[1] == "6"):
                 items = []
                 for i in range(2, len(current_packet), 2):
                     if int(current_packet[i + 1]) != -1:
--- plugins/recipe_plugin.py
+++ plugins/recipe_plugin.py
@@ -14,8 +14,12 @@ class RecipePlugin(Plugin):
         recipes = recipe_parser.insert_recipes(packet_list)
 
         recipe_groups = defaultdict(list)
+        other_groups = defaultdict(list)
         for recipe in recipes:
-            recipe_groups[recipe.producer_map_npc_id].append(recipe)
+            if recipe.producer_map_npc_id:
+                recipe_groups[recipe.producer_map_npc_id].append(recipe)
+            else:
+                other_groups[recipe.producer_item_vnum].append(recipe)
 
         output_recipes_dir = "./output/recipes"
         if os.path.exists(output_recipes_dir):
@@ -23,11 +27,16 @@ class RecipePlugin(Plugin):
         os.makedirs(output_recipes_dir)
 
         for npc_id, recipes in recipe_groups.items():
-            if npc_id is not None:
-                yaml_data = self.recipes_to_yaml(recipes)
-                file_path = os.path.join(output_recipes_dir, f"npc_{npc_id}_recipes.yaml")
-                with open(file_path, "w") as yaml_file:
-                    yaml_file.write(yaml_data)
+            yaml_data = self.recipes_to_yaml(recipes)
+            file_path = os.path.join(output_recipes_dir, f"npc_{npc_id}_recipes.yaml")
+            with open(file_path, "w") as yaml_file:
+                yaml_file.write(yaml_data)
+
+        for item_vnum, recipes in other_groups.items():
+            yaml_data = self.recipes_to_yaml(recipes)
+            file_path = os.path.join(output_recipes_dir, f"item_{item_vnum}_recipes.yaml")
+            with open(file_path, "w") as yaml_file:
+                yaml_file.write(yaml_data)
 
         print("Recipes parsed successfully !")
 
@@ -37,9 +46,14 @@ class RecipePlugin(Plugin):
             recipe_dict = {
                 "item_vnum": dto.item_vnum,
                 "quantity": dto.quantity,
-                "producer_map_npc_id": dto.producer_map_npc_id,
-                "producer_item_vnum": dto.producer_item_vnum,
                 "items": dto.items
             }
+
+            if dto.producer_map_npc_id is not None and dto.producer_map_npc_id > 0:
+                recipe_dict["producer_map_npc_id"] = dto.producer_map_npc_id
+            
+            if dto.producer_item_vnum is not None and dto.producer_item_vnum > 0:
+                recipe_dict["producer_item_vnum"] = dto.producer_item_vnum
+
             yaml_list.append(recipe_dict)
         return yaml.dump({"recipes": yaml_list}, default_flow_style=False, sort_keys=False)
