60f31ba72a562f3a691998903e4537124e354ddf
Fizo55 <hugomacees56000@gmail.com>
2024-05-10 23:24:43 +0200

    chore: fix music field

--- dto/map_dto.py
+++ dto/map_dto.py
@@ -1,11 +1,11 @@
 class MapDto:
-    def __init__(self, map_id: int, music: int, flags: list):
+    def __init__(self, map_id: int, map_music_id: int, flags: list):
         self.map_id = map_id
         self.map_vnum = map_id
         self.map_name_id = map_id
-        self.music = music
+        self.map_music_id = map_music_id
         self.flags = flags
 
     def __repr__(self):
-        return f"MapDto(map_id={self.map_id}, map_vnum={self.map_vnum}, music={self.music}, " \
+        return f"MapDto(map_id={self.map_id}, map_vnum={self.map_vnum}, map_music_id={self.map_music_id}, " \
                f"flags={self.flags})"
