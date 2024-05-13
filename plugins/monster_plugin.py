df3d8f9e873863815d6b888be71dd05291e2030f
Fizo55 <hugomacees56000@gmail.com>
2024-05-13 03:01:44 +0200

    Update monster_plugin.py

--- plugins/monster_plugin.py
+++ plugins/monster_plugin.py
@@ -12,7 +12,7 @@ class MonsterPlugin(Plugin):
 
     async def process(self, cleaned_lines):
         cleaned_lines_filtered = [line for line in cleaned_lines if line]
-        packet_list = [line for line in cleaned_lines_filtered if line[0] in ("in", "mv", "at")]
+        packet_list = [line for line in cleaned_lines_filtered if line[0] in ("in", "mv", "c_map")]
 
         monster_parser = MonsterParser()
         monster_groups = monster_parser.insert_monsters(packet_list)
